bc8e2c71a8ab5b2d9ba9bbacfc7be24a0537fb0e
xiaoyu00615 <daizy2005@qq.com>
2025-07-28 21:34:40 +0800

    write homePage in progress

---
+++ src/components/AnswerQuestionsCard.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+  defineProps({
+    frontColor:{
+      type:String,
+      default:'#b2d642'
+    },
+    backColor:{
+      type:String,
+      default:'#42b2d6'
+    }
+  })
+</script>
+
+<template>
+  <div class="item">
+    <div class="card">
+      <div class="front" :style="{background: frontColor}">
+        <h3>
+          <slot name="front-title">普通答题</slot>
+        </h3>
+        <p>
+          <slot name="front-content">依据选定文档进行答题，内有计时，判断答题情况等功能使用。</slot>
+        </p>
+      </div>
+      <div class="back" :style="{background: backColor}">
+        <h3><slot name="back-title">注意事项</slot></h3>
+        <p><slot name="back-content">进入后会开始计时，直接退出会失去答题进度（建议提交后退出！）</slot></p>
+        <h2 class="back-line" :style="{
+          borderStyle : 'dashed',
+          borderWidth : '2px',
+          borderColor : backColor
+         }">开始 GO!</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+   .item {
+    width: 200px;
+    height: 300px;
+    overflow: hidden;
+    position: relative;
+  }
+   .item h3{
+    line-height: 40px;
+    border-bottom: 3px solid #000;
+    margin-bottom: 15px;
+  }
+   .item p{
+    text-indent: 2em;
+    padding: 0 5px;
+  }
+   .item h2{
+    cursor: pointer;
+    text-align: center;
+    line-height: 50px;
+    margin-top: 40px;
+  }
+    .item h2:hover{
+    border: 2px dashed #fff !important;
+  }
+    .item .card{
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+    transition: transform .5s ease-in-out;
+  }
+    .item .card .front,
+    .item .card .back{
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    position: absolute;
+    padding: 15px;
+    font-size: 16px;
+    text-align: left;
+  }
+    .item .card:hover{
+    transform: rotateY(180deg);
+  }
+    .item .card .front{
+    border-top: 3px solid #333;
+    border-left: 5px solid #333;
+
+    border-bottom: 5px solid #555;
+    border-right: 3px solid #999;
+
+  }
+
+    .item .card .back{
+    transform: rotateY(180deg);
+    border-top: 3px solid #333;
+    border-left: 5px solid #333;
+
+    border-bottom: 5px solid #555;
+    border-right: 3px solid #999;
+
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,105 @@
 <script setup lang="ts">
+  import {reactive} from "vue";
+
+  import AnswerQuestionsCard from "@/components/AnswerQuestionsCard.vue";
+  const fileData = reactive(null)
 
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div class="container">
+    <div class="left">
+      <div class="int-content">
+        <p>添加数据文件：</p>
+        <input class="int-file" type="file">
+        <button>提交文件</button>
+      </div>
+
+      <div class="file-data">
+        <h2>文件数据:</h2>
+        <p v-if="fileData">暂时没有文件信息</p>
+        <div v-else class="file-message">
+          <p>文件名称：</p>
+          <p>文件大小：</p>
+          <p>文件格式：</p>
+        </div>
+      </div>
+    </div>
+    <div class="right">
+      <div class="column">
+
+      </div>
+      <div class="content">
+        <AnswerQuestionsCard></AnswerQuestionsCard>
+        <AnswerQuestionsCard front-color="#775ad6" back-color="#5ad6b5">
+          <template #front-title>自定义答题</template>
+          <template #front-content>依据自己设计的题进行答题，统计时间，判断答题情况等，</template>
+          <template #back-title>注意事项</template>
+          <template #back-content>进入后会开始计时，直接退出会失去答题进度（建议提交后退出！）</template>
+        </AnswerQuestionsCard>
+        <AnswerQuestionsCard front-color="#de315d" back-color="#de31b4">
+          <template #front-title>错题本答题</template>
+          <template #front-content>依据自己平时答题目错误的题进行答题，统计时间，判断答题情况等，</template>
+        </AnswerQuestionsCard>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+  .container{
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+  }
+  .container .left{
+    padding: 20px;
+    width: 30%;
+    border:2px solid #000;
+    height: 100%;
+  }
+  .container .left .int-content p{
+    margin: 10px 0 ;
+    font-size: 20px;
+  }
+  .container .left .int-content .int-file{
+    width: 300px;
+    height: 45px;
+    cursor: pointer;
+  }
+  .container .left .int-content button{
+    width: 90px;
+    height:40px;
+    border-radius: 11px;
+    cursor: pointer;
+  }
+  .container .left .int-content button:hover{
+    background: #ccc;
+  }
+  .container .file-data h2{
+    line-height: 45px;
+  }
+  .container .file-data .file-message{
+    line-height: 35px;
+  }
 
+
+  .container .right{
+    width: 70%;
+    height: 100%;
+    border:2px solid red;
+    padding: 20px;
+  }
+  .container .column{
+    height: 60px;
+    margin-bottom: 20px;
+  }
+
+  .container .right .content{
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    gap: 50px;
+    justify-items: center;
+  }
 </style>
